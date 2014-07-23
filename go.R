f342a4ec2a2436ab94bbba49ad229265b861a6ce
Chris Friedline <cfriedline@vcu.edu>
2014-07-23 19:06:39 -0400

    store description in go object

--- go.R
+++ go.R
@@ -15,17 +15,20 @@ go = read.table(go_file, header=F, sep="\t")
 gene_id_2go <- readMappings(file="all_annot_GOs_20140722_2254.txt_topGO.txt")
 
 interesting = list()
-interesting[[1]] = cambium_interesting
-interesting[[2]] = needle_interesting
+interesting$cambium = cambium_interesting
+interesting$needle = needle_interesting
+godata = list()
 for (i in 1:length(interesting)) {
     interest = interesting[[i]]
     gene_list <- factor(as.integer(gene_names %in% names(interest)))
     names(gene_list) <- gene_names
 
-    GOdata = new("topGOdata", 
+    GOdata = new("topGOdata",
+             description=names(interesting)[i],
              ontology = "MF", 
              allGenes = gene_list, 
              annot = annFUN.gene2GO, 
              gene2GO = gene_id_2go)
+    godata[[i]] = GOdata
 }
 
