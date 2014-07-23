fe5e0a68d057acb72a462e97fca4a5ec7768a544
Chris Friedline <cfriedline@vcu.edu>
2014-07-23 00:04:22 -0400

    split off tissue and parent, calc norm factors (missing from previous)

--- dge.R
+++ dge.R
@@ -1,10 +1,15 @@
 f = "~/projects/black_spruce/seqclean/all_ests.fa.clean_output/contig_member.counts"
 counts = read.table(f, header=T, row.names=1)
+tissue = c("cambium", "cambium", "needle", "needle")
+parent = c("mom","dad","mom","dad")
 env = data.frame(row.names=colnames(counts), 
-                    tissue=c("cambium", "cambium", "needle", "needle"),
-                    parent=c("mom","dad","mom","dad"))
-design = model.matrix(~env$parent + env$tissue)
+                    tissue=tissue,
+                    parent=parent)
+design = model.matrix(~parent + tissue)
+rownames(design)=colnames(counts)
+print(design)
 y = DGEList(counts=counts)
+y = calcNormFactors(y)
 y = estimateGLMCommonDisp(y, design, verbose=T)
 y = estimateGLMTrendedDisp(y, design)
 y = estimateGLMTagwiseDisp(y, design)
