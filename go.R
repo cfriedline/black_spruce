9761e67061d4c585b6510c4b07df5e77bda453be
Chris Friedline <cfriedline@vcu.edu>
2014-07-25 17:11:04 -0400

    write top20 mappings to files

--- go.R
+++ go.R
@@ -1,4 +1,5 @@
 library(topGO)
+rm(list=ls())
 count_file = "seqclean/all_ests.fa.clean_output/contig_member.counts"
 counts = read.table(count_file, header=T, row.names=1, sep="\t")
 
@@ -19,6 +20,7 @@ interesting$cambium = cambium_interesting
 interesting$needle = needle_interesting
 godata = list()
 onts = c("BP","CC", "MF")
+sigs = list()
 for (i in 1:length(onts)) {
     for (j in 1:length(interesting)) {
         interest = interesting[[j]]
@@ -26,13 +28,25 @@ for (i in 1:length(onts)) {
         names(gene_list) <- gene_names
         
         GOdata = new("topGOdata",
-                     description=paste(names(interesting)[j], onts[i], sep="/"),
+                     description=paste(names(interesting)[j], onts[i], sep="-"),
                      ontology = onts[i], 
                      allGenes = gene_list, 
                      annot = annFUN.gene2GO, 
                      gene2GO = gene_id_2go,
                      nodeSize=5)
-        godata[[i]] = GOdata
+        print(GOdata)
+        godata = append(godata, GOdata)
+        test.stat = runTest(GOdata, algorithm = "classic", statistic = "fisher")
+        sigs = append(sigs, test.stat)
+        printGraph(GOdata, 
+                   test.stat, 
+                   firstSigNodes = 5, 
+                   fn.prefix = paste("tGO", "for", description(GOdata)), 
+                   useInfo = "all", 
+                   pdfSW = TRUE)
+        
+        gt = GenTable(GOdata, classic=test.stat, topNodes=20)
+        write.table(gt, file=paste(description(GOdata), ".txt", sep=""), row.names=F)
     }    
 }
 
