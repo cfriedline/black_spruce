b76f04f20a1f894b483c5b9e8c7fd0d0438455d4
Chris Friedline <cfriedline@vcu.edu>
2014-07-25 15:28:46 -0400

    compute DGE batch model for each tissue type clear env at start

--- dge.R
+++ dge.R
@@ -1,4 +1,5 @@
 library(edgeR)
+rm(list=ls())
 f = "~/projects/black_spruce/seqclean/all_ests.fa.clean_output/contig_member.counts"
 counts = read.table(f, header=T, row.names=1)
 tissue = factor()
@@ -10,7 +11,7 @@ for (k in 1:ncol(counts)) {
     } else {
         parent = c(parent, "dad")
     }
-    
+
     if (length(i <- grep("N", c))) {
         tissue = c(tissue, "needle")
     } else {
@@ -19,19 +20,34 @@ for (k in 1:ncol(counts)) {
 }
 tissue = as.factor(tissue)
 parent = as.factor(parent)
-env = data.frame(row.names=colnames(counts), 
+env = data.frame(row.names=colnames(counts),
                     tissue=tissue,
                     parent=parent)
-design = model.matrix(~parent + tissue, env)
-rownames(design)=colnames(counts)
-print(design)
-y = DGEList(counts=counts)
-y = calcNormFactors(y)
-y = estimateGLMCommonDisp(y, design, verbose=T)
-y = estimateGLMTrendedDisp(y, design)
-y = estimateGLMTagwiseDisp(y, design)
-fit = glmFit(y, design)
-lrt = glmLRT(fit)
-tt = topTags(lrt, n=nrow(counts))
-print(tt[1:10,])
-write.csv(tt, file="~/projects/black_spruce/toptags.csv")
+DENameList = list()
+for (i in 1:length(levels(tissue))) {
+    ref = levels(tissue)[i]
+    tissue = relevel(tissue, ref=ref)
+    design = model.matrix(~tissue+parent)
+    rownames(design)=colnames(counts)
+    print(design)
+
+    y = DGEList(counts=counts,group=tissue)
+    y = calcNormFactors(y)
+    plotMDS(y)
+    y = estimateGLMCommonDisp(y, design)
+    y = estimateGLMTrendedDisp(y,design)
+    y = estimateGLMTagwiseDisp(y,design)
+    plotBCV(y)
+    fit = glmFit(y, design)
+    lrt = glmLRT(fit,coef=2)
+    tt = topTags(lrt, n=150)
+    print(tt[1:10,])
+    print(summary(dt <- decideTestsDGE(lrt)))
+    isDE <- as.logical(dt)
+    DEnames <- rownames(y)[isDE]
+    DENameList[[i]] = DEnames
+    write.csv(tt[DEnames,], file=sprintf("~/projects/black_spruce/toptags_%s.csv",ref))
+}
+DEunion = union(DENameList[[1]], DENameList[[2]])
+DEintersect = intersect(DENameList[[1]], DENameList[[2]])
+DEdiff = setdiff(DENameList[[1]], DENameList[[2]])
